1d8abde665641561126b61a69f034d5e17a7cc69
milanbc <milanjayasinghe1234@gmail.com>
2021-12-18 18:49:27 +0530

    Add zitem page create

---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" style="font-size: xx-large;" href="/">Lak Derana Liqour Bar <i class="fa fa-glass" aria-hidden="true"></i></a>
+        <img src="img/logo.png" style="width: 5%;" alt="">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="text-uppercase justify-content-end collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+           
+            <li class="nav-item">
+              <a class="nav-link " href="/">Item Add</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/">Supplier Manage</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/">Reports</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/">About Us</a>
+            </li>
+           
+           </title>
+</head>
+<body>
+    
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('School');
 });
+
+Route::get('/ItemAdd', function () {
+    return view('Itemadd');
+});
