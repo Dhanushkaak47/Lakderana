0e032e3a79ddde122791a3f7def226e709185f80
Malshan Hansaka <75769208+malshanHD@users.noreply.github.com>
2022-01-04 17:47:45 +0530

    reservation invoice

--- app/Http/Controllers/paymentHadling.php
+++ app/Http/Controllers/paymentHadling.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\res_service;
+use Carbon\Carbon;
 use App\Models\reservation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Barryvdh\DomPDF\Facade as PDF;
 
 class paymentHadling extends Controller
 {
@@ -28,19 +30,37 @@ class paymentHadling extends Controller
     {
         # code...
         $roomprice=DB::table('rooms')
-        ->select('roomtypes.price')
+        ->select('roomtypes.price','rooms.roomNo')
         ->join('roomtypes','roomtypes.id','=','rooms.roomtype')
         ->where('rooms.roomNo',$roomnumber)
         ->get();
 
-        $date = res_service::where('id',$id)->get();
+        $date = reservation::where('id',$id)->get();
+
+        $currmonth=Carbon::now();
+        $lastMonth =  $currmonth->format('m-d-Y');
+        
 
         $services=DB::table('res_services')
-        ->select('res_services.*','dinings.price')
+        ->select('res_services.qty','dinings.*')
         ->join('dinings','dinings.id','=','res_services.service')
         ->where('res_services.reservation_id',$id)
         ->get();
 
-        dd($date);
+        
+
+        $customer=DB::table('reservations')
+        ->select('customer_data.*')
+        ->join('customer_data','customer_data.id','=','reservations.cus_id')
+        ->where('reservations.id',$id)
+        ->get();
+
+        // $pdf = PDF::loadView('pay.paymentdetail',compact('roomprice','services','date','customer','lastMonth'))->setOptions(['defaultFont' => 'sans-serif']);
+
+        // $date = Carbon::today()->toDateString();
+        // return $pdf->download('Invoice'.$date.'.pdf');
+
+        return view('pay.paymentdetail', compact('roomprice','services','date','customer','lastMonth'));
+        
     }
 }
