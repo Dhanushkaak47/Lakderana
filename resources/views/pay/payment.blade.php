f899501daffdbbdc7198c94b66cc09fbb04392a4
Dhanushka ChanakaMadhusanka <dhanushkaak47@gmail.com>
2022-01-14 19:35:19 +0530

    Change name
    Payment handling

--- resources/views/pay/payment.blade.php
+++ resources/views/pay/payment.blade.php
@@ -20,7 +20,7 @@
     <div class="container mt-3">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="title text-center font-weight-bold">Payments Hadeling</h3>
+                <h3 class="title text-center font-weight-bold">Payments Handeling</h3>
             </div>
         </div>
     </div>
